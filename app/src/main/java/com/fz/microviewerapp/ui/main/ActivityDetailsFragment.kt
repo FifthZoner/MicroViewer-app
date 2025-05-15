5b77df0642002e0e27bb622d50127c3fcb8fca01
Szymon Jabłoński <szyfzjablonski@gmail.com>
2025-05-15 22:02:35 +0200

    Add: detail data list

--- app/src/main/java/com/fz/microviewerapp/ui/main/ActivityDetailsFragment.kt
+++ app/src/main/java/com/fz/microviewerapp/ui/main/ActivityDetailsFragment.kt
@@ -2,32 +2,25 @@ package com.fz.microviewerapp.ui.main
 
 import android.content.Intent
 import android.graphics.BitmapFactory
-import androidx.fragment.app.viewModels
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.LinearLayout
-import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.fz.microviewerapp.ActivityDetails
 import com.fz.microviewerapp.ApiAddress
-
-import com.fz.microviewerapp.R
-import com.fz.microviewerapp.databinding.ActivityActivityDetailsBinding
 import com.fz.microviewerapp.databinding.FragmentMainBinding
-import com.fz.microviewerapp.databinding.FragmentManufacturersBoardListBinding
-import com.fz.microviewerapp.ui.manufacturers.ManufacturersBoardList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonArray
-import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
 import java.net.URL
+import androidx.core.net.toUri
+
 
 class ActivityDetailsFragment : Fragment() {
 
@@ -75,6 +68,52 @@ class ActivityDetailsFragment : Fragment() {
                 // now let's parse the json
                 val json = Json {ignoreUnknownKeys = true}.parseToJsonElement(result).jsonObject;
                 try {
+                    binding.chipText.text = json["chi_name"].toString().removeSurrounding("\"");
+                    binding.manufacturerText.text = json["man_name"].toString().removeSurrounding("\"");
+                    binding.categoryText.text = json["cat_name"].toString().removeSurrounding("\"");
+                    binding.nameText.text = json["boa_name"].toString().removeSurrounding("\"");
+
+                    val pin_link = json["boa_pin"].toString().removeSurrounding("\"");
+                    if (pin_link != "" && pin_link != "null") {
+                        val button : Button = Button(context);
+                        button.text = "Pinout"
+                        button.isAllCaps = false;
+                        button.setOnClickListener {
+                            startActivity(Intent(Intent.ACTION_VIEW, pin_link.toUri()))
+                        }
+                        binding.items.addView(button)
+                    }
+                    val sch_link = json["boa_sch"].toString().removeSurrounding("\"");
+                    if (sch_link != "" && sch_link != "null") {
+                        val button : Button = Button(context);
+                        button.text = "Schematics"
+                        button.isAllCaps = false;
+                        button.setOnClickListener {
+                            startActivity(Intent(Intent.ACTION_VIEW, sch_link.toUri()))
+                        }
+                        binding.items.addView(button)
+                    }
+                    val doc_link = json["boa_doc"].toString().removeSurrounding("\"");
+                    if (doc_link != "" && doc_link != "null") {
+                        val button : Button = Button(context);
+                        button.text = "Documentation"
+                        button.isAllCaps = false;
+                        button.setOnClickListener {
+                            startActivity(Intent(Intent.ACTION_VIEW, doc_link.toUri()))
+                        }
+                        binding.items.addView(button)
+                    }
+                    val size_link = json["boa_size"].toString().removeSurrounding("\"");
+                    if (size_link != "" && size_link != "null") {
+                        val button : Button = Button(context);
+                        button.text = "Size"
+                        button.isAllCaps = false;
+                        button.setOnClickListener {
+                            startActivity(Intent(Intent.ACTION_VIEW, size_link.toUri()))
+                        }
+                        binding.items.addView(button)
+                    }
+
                     val link = json["boa_image"].toString().removeSurrounding("\"")
                     val bytes = withContext(Dispatchers.IO) {
                         URL(link).readBytes()
